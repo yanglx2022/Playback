4b6f1ccaf960e008aaef565ed555ea50d4278444
YangDR <ydrml@hotmail.com>
2019-03-14 11:59:50 +0800

    clean up

--- Playback/Packet.cs
+++ Playback/Packet.cs
@@ -6,11 +6,6 @@ namespace Playback {
 	/// </summary>
 	public class Packet {
 		/// <summary>
-		///     数据可用(有时间戳)
-		/// </summary>
-		public readonly bool Checked;
-
-		/// <summary>
 		///     原始文本数据
 		/// </summary>
 		public readonly string Text;
@@ -20,15 +15,22 @@ namespace Playback {
 		/// </summary>
 		public readonly long TimeStamp;
 
-		/// <summary>
-		///     一次回放的数据
-		/// </summary>
-		public Packet(string text) {
-			var head = new string(text.TakeWhile(char.IsDigit).ToArray());
+		private Packet(string text, long timeStamp) {
+			Text      = text;
+			TimeStamp = timeStamp;
+		}
 
-			Checked   = long.TryParse(head, out var value);
-			TimeStamp = Checked ? value : 0;
-			Text      = new string(text.Skip(head.Length + 1).ToArray());
+		/// <summary>
+		/// 	尝试解析一个数据	
+		/// </summary>
+		/// <param name="text">行</param>
+		/// <param name="packet">解析成功数据</param>
+		/// <returns></returns>
+		public static bool TryParse(string text, out Packet packet) {
+			var head  = new string(text.TakeWhile(char.IsDigit).ToArray());
+			var check = long.TryParse(head, out var value);
+			packet = check ? new Packet(new string(text.Skip(head.Length + 1).ToArray()), value) : default;
+			return check;
 		}
 
 		/// <summary>
